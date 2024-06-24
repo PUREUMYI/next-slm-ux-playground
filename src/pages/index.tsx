54af50fa65caee7e6365c67e8cc7e88031f83159
Julio Kim <julio@pnoni.com>
2024-06-24 23:32:38 +0900

    style added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
+      className={"flex min-h-screen flex-col items-center justify-between p-24"}
     >
-      <h1>Hello World</h1>
+      <h3 className={"text-3xl font-bold underline"}>Hello World</h3>
       <Button>Click me</Button>
     </main>
   )
