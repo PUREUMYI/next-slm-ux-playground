fad3f4633a66cb5d61930013280db418e9cb0a21
Julio Kim <julio@pnoni.com>
2024-07-02 20:56:08 +0900

    * MessageModel 오류 수정 (handle 추가)

--- src/components/playground/MessageModel.tsx
+++ src/components/playground/MessageModel.tsx
@@ -1,10 +1,13 @@
 import { BotMessageSquare, Copy } from "lucide-react";
+import { useState } from "react";
 import { SlmMessageType } from "../models/message";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
 
 export const MessageModel = ({ slmMessage }) => {
+  const [message, setMessage] = useState(slmMessage.message);
+
   return (
     <div className="relative flex items-center mb-4 gap-4">
       {slmMessage.role == SlmMessageType.HUMAN ? (
@@ -23,7 +26,8 @@ export const MessageModel = ({ slmMessage }) => {
           placeholder="Type your message here..."
           className="min-h-12 h-full resize-none border-0 p-3 shadow-none focus-visible:ring-0"
           // onInput={handleTextareaHeight}
-          value={slmMessage.message}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         />
       </div>
     </div>
